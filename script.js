8667d161509a764c384861f1ab2cddced42cffcf
Rafaela Reis <rafaelareis7@hotmail.com>
2024-03-26 19:07:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMode () {
+   const html = document.documentElement
+   /* if(html.classList.contains("light") {
+      html.classList.remove("light") }
+    else {
+      html.classList.add("light") 
+    } (uma outra forma de fazer mais elaborada)*/
+
+    html.classList.toggle("light")
+
+    /* Pegar a tag img */
+    const img = document.querySelector("#progile img")
+   
+   /*substituir a imagem*/
+    if(html.classList.contains("light"))
+    /* Se tiver  light mode, adicionar imagem lighy */
+        img.setAttribute("src","./assets/avatar-light.png")
+    else {
+    /* Se tiver sem light mode mantem imagem normal*/
+    img.setAttribute("src","./assets/avatar.png")
+    }   
+}
